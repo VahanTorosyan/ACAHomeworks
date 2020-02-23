cbe4a936f2e2a5f80b1fde1b0b0084987d490ad7
Vahan <vah.torosyan@gmail.com>
2020-02-23 18:55:53 +0400

    Problem 2 - Done

---
+++ src/am/vtorosyan/Problem2.java
@@ -0,0 +1,14 @@
+package am.vtorosyan;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        System.out.println("Please find the sum of first 20 natural numbers");
+        int i = 1;
+        int sum = 0;
+        while (i <= 20) {
+            sum = sum + i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
