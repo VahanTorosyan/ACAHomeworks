16d6cbf4b65324d40b51760c861b950d6dc9dd6d
Vahan <vah.torosyan@gmail.com>
2020-02-23 18:51:02 +0400

    Problem 1 - Done

--- src/am/vtorosyan/Problem1.java
+++ src/am/vtorosyan/Problem1.java
@@ -1,4 +1,12 @@
 package am.vtorosyan;
 
 public class Problem1 {
+    public static void main(String[] args) {
+        System.out.println("Please find below first 50 natural numbers");
+        int i = 1;
+        while (i <= 50) {
+            System.out.println(i);
+            i++;
+        }
+    }
 }
