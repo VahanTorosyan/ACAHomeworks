271a7440289e0d191134b47cee45a2a05e22c3e2
Vahan <vah.torosyan@gmail.com>
2020-02-24 12:53:04 +0400

    Problem 5 - Done + SUM of numbers

--- src/am/vtorosyan/Homework4/Problem6.java
+++ src/am/vtorosyan/Homework4/Problem6.java
@@ -1,7 +1,13 @@
 package am.vtorosyan.Homework4;
 
+import java.util.Scanner;
+
 public class Problem6 {
     public static void main(String[] args) {
+        System.out.println("Input an integer n. The program will find the following sum: 1 + ½ + ⅓ +¼ + … 1/n ");
+        Scanner scanner = new (System.in);
+        System.out.print("Input: ");
+        int n = scanner.nextInt();
 
     }
 }
