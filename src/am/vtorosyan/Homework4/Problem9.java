5d5c39c313f13805716ed0056bbdb6094565ee40
VahanTorosyan <vah.torosyan@gmail.com>
2020-02-24 14:56:14 +0400

    Problem9 - Done

---
+++ src/am/vtorosyan/Homework4/Problem9.java
@@ -0,0 +1,19 @@
+package am.vtorosyan.Homework4;
+
+import java.util.Scanner;
+
+public class Problem9 {
+    public static void main(String[] args) {
+        System.out.println("Input an integer and program will calculate factorial.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input number: ");
+        long n = scanner.nextLong();
+        int i = 1;
+        long result = 1;
+        while (i <= n) {
+            result *= i;
+            i++;
+        }
+        System.out.println("Output: " + n + "!" + " = " + result);
+    }
+}
