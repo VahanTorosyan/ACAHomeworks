509b3e42da7b37a0af94166275a4fde68a1bd94a
VahanTorosyan <vah.torosyan@gmail.com>
2020-02-24 13:32:32 +0400

    Problem7 - Done

--- src/am/vtorosyan/Homework4/Problem7.java
+++ src/am/vtorosyan/Homework4/Problem7.java
@@ -1,7 +1,23 @@
 package am.vtorosyan.Homework4;
 
+import java.util.Scanner;
+
 public class Problem7 {
     public static void main(String[] args) {
-
+        System.out.println("Input an integer and program will calculate the result of the following formula:\n" + "1 - 2 + 3 - 4 + 5 - 6 + 7 .... (+or-) n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int n = scanner.nextInt();
+        int sum = 0;
+        int i = 1;
+        while (i <= n) {
+            if (i % 2 == 0) {
+                sum -= i;
+            } else {
+                sum +=i;
+            }
+            i++;
+        }
+        System.out.println("Output: " + sum);
     }
 }
