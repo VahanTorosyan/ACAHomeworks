7ff2d3c8514792bd0512e642d8ec435a10a26b7f
VahanTorosyan <vah.torosyan@gmail.com>
2020-02-24 13:17:55 +0400

    Problem6 - Done

--- src/am/vtorosyan/Homework4/Problem6.java
+++ src/am/vtorosyan/Homework4/Problem6.java
@@ -5,9 +5,16 @@ import java.util.Scanner;
 public class Problem6 {
     public static void main(String[] args) {
         System.out.println("Input an integer n. The program will find the following sum: 1 + ½ + ⅓ +¼ + … 1/n ");
-        Scanner scanner = new (System.in);
+        Scanner scanner = new Scanner (System.in);
         System.out.print("Input: ");
         int n = scanner.nextInt();
+        double i = 1;
+        double sum = 0;
+        while (i <= n) {
+            sum += 1/i;
+            i++;
+        }
+        System.out.println("Output: " + sum);
 
     }
 }
