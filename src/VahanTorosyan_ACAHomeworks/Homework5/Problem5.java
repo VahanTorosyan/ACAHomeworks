4e5e0c8a5479aab820dd450984d26a0a0a502b69
Vahan <vah.torosyan@gmail.com>
2020-03-02 22:39:10 +0400

    Homework5 Problem5 - Done

---
+++ src/VahanTorosyan_ACAHomeworks/Homework5/Problem5.java
@@ -0,0 +1,15 @@
+package VahanTorosyan_ACAHomeworks.Homework5;
+
+import java.util.Scanner;
+
+public class Problem5 {
+    public static void main(String[] args) {
+        char[] array = new char[]{'a', 'a', 'a', 'a', 'a', 'a', 'a'};
+        int couter = 0;
+        for (int i = 0; i < array.length; i++) {
+            couter++;
+        }
+        System.out.println(couter);
+
+    }
+}
