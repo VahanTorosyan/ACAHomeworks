a4b0ca26a5ca5e3130c39be32a031aba1057131d
Vahan <vah.torosyan@gmail.com>
2020-03-02 14:39:01 +0400

    Finish Homework5 - Problem1

---
+++ src/VahanTorosyan_ACAHomeworks/Homework5/Problem2.java
@@ -0,0 +1,13 @@
+package VahanTorosyan_ACAHomeworks.Homework5;
+
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        System.out.print("Input size of array: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+
+    }
+}
