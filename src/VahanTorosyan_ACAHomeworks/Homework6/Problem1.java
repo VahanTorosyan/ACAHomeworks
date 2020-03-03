4bf608dd773ead12446f75f752f6d810ef5a57a5
Vahan <vah.torosyan@gmail.com>
2020-03-04 00:53:17 +0400

    Homework6 Done Problem1

--- src/VahanTorosyan_ACAHomeworks/Homework6/Problem1.java
+++ src/VahanTorosyan_ACAHomeworks/Homework6/Problem1.java
@@ -1,4 +1,20 @@
 package VahanTorosyan_ACAHomeworks.Homework6;
 
+import java.util.Scanner;
+
 public class Problem1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a string: ");
+        String word = scanner.next();
+        System.out.print("Input an index: ");
+        int k = scanner.nextInt();
+        if (k <= word.length()){
+            char symbol = word.charAt(k);
+            System.out.println("Character at index " + k + " is " + symbol);
+        } else {
+            System.out.println("out of bounds");
+        }
+
+    }
 }
