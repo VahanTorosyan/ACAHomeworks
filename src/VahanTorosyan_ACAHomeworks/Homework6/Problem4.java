dbdc52aef3b9e84408538fbf9b7afc5c3df3bd0c
VahanTorosyan <vah.torosyan@gmail.com>
2020-03-05 15:58:45 +0400

    Homework 6 Finish Problem3

---
+++ src/VahanTorosyan_ACAHomeworks/Homework6/Problem4.java
@@ -0,0 +1,45 @@
+package VahanTorosyan_ACAHomeworks.Homework6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem4 {
+    public static void main(String[] args) {
+        System.out.print("Input a string: ");
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next();
+        System.out.print("Input number of rotations: ");
+        int n = scanner.nextInt();
+       /*
+        char tmp='';
+        char[] ch = new char[word.length()];
+        for (int i = 0; i < word.length(); i++){
+            //ch[i] = word.charAt(i);
+
+        }
+
+
+
+        */
+    }
+}
+
+
+/*
+for (int i=0; i < ch.length; i++ ){
+            System.out.print(ch[i]);
+        }
+        for (int i = 0; i < n; i++){
+            char tmp = ch[ch.length - 1 - i];
+            for (int j = ch.length-1; j > 0; j--){
+                ch[j] = ch[j-1];
+
+            }
+            ch[0] = tmp;
+
+        }
+        System.out.println();
+    for (int i=0; i < ch.length; i++ ){
+        System.out.print(ch[i]);
+    }
+ */
