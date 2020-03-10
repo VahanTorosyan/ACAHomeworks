1b4c2786bffd6bec1be8ab512e46f40628f1337d
Vahan <vah.torosyan@gmail.com>
2020-03-10 14:05:59 +0400

    Howmework 6 Problem 6 done

--- src/VahanTorosyan_ACAHomeworks/Homework6/Problem6.java
+++ src/VahanTorosyan_ACAHomeworks/Homework6/Problem6.java
@@ -1,7 +1,29 @@
 package VahanTorosyan_ACAHomeworks.Homework6;
 
+import java.util.Scanner;
+
 public class Problem6 {
     public static void main(String[] args) {
+        System.out.println("Please enter '(' and ')' ) parenthesis only and program will Print \"valid\" if the parenthesis are balanced, and print \"invalid\" otherwise");
+        System.out.print("Input parenthesis sequence : ");
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        int balance = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                balance++;
+            }else {
+                balance--;
+            }
 
+            if(balance < 0){
+                System.out.println("Invalid");
+                break;
+            }
+
+        }
+        if (balance == 0) {
+            System.out.println("Valid");
+        }
     }
 }
